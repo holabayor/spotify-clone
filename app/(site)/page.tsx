ff764b136fe4cdfde6b2d6ece165d259b7d554cd
holabayor <liasu.olabayo@gmail.com>
2023-06-16 11:57:42 +0100

    upload

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -1,7 +1,13 @@
+import getSongs from '@/actions/getSongs';
 import Header from '@/components/Header';
 import ListItem from '@/components/ListItem';
+import PageContent from '@/components/PageContent';
 
-export default function Home() {
+export const revalidate = 0;
+
+export default async function Home() {
+  const songs = await getSongs();
+
   return (
     <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
       <Header>
@@ -16,9 +22,11 @@ export default function Home() {
       </Header>
       <div className="mt-2 mb-7 px-6">
         <div className="flex justify-between items-center">
-          <h1 className="text-white text-2xl font-semibold">Newest songs</h1>
+          <h1 className="text-white text-2xl font-semibold">
+            Newest songs
+          </h1>
         </div>
-        <div>List of songs</div>
+        <PageContent songs={songs} />
       </div>
     </div>
   );
