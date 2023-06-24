d23f37339fb0a700d03b4bc3476de19cb458b5f2
holabayor <liasu.olabayo@gmail.com>
2023-06-24 20:24:12 +0100

    stripe webhook

--- app/api/webhooks/routes.ts
+++ app/api/webhooks/routes.ts
@@ -3,3 +3,79 @@ import { NextResponse } from 'next/server';
 import { headers } from 'next/headers';
 
 import { stripe } from '@/libs/stripe';
+import {
+  upsertProductRecord,
+  upsetPriceRecord,
+  manageSubscriptionStatusChange,
+} from '@/libs/supabaseAdmin';
+
+const relevantEvents = new Set([
+  'product.created',
+  'product.updated',
+  'price.created',
+  'price.updated',
+  'checkout.session.completed',
+  'customer.subscription.created',
+  'customer.subscription.updated',
+  'customer.subscription.deleted',
+]);
+
+export async function POST(request: Request) {
+  const body = await request.text();
+  const sig = headers().get('Stripe-Signature');
+
+  const webhookSecret = process.env.STRIPE_WEBHOOK_SECRET;
+  let event: Stripe.Event;
+
+  try {
+    if (!sig || !webhookSecret) return;
+    event = stripe.webhooks.constructEvent(body, sig, webhookSecret);
+  } catch (error: any) {
+    console.log(`Webhook Error: ${error.message}`);
+    return new NextResponse(`Webhook error: ${error.message}`, { status: 400 });
+  }
+
+  if (!relevantEvents.has(event.type)) {
+    try {
+      switch (event.type) {
+        case 'product.created':
+        case 'product.updated':
+          await upsertProductRecord(event.data.object as Stripe.Product);
+          break;
+        case 'price.created':
+        case 'price.updated':
+          await upsetPriceRecord(event.data.object as Stripe.Price);
+        case 'customer.subscription.created':
+        case 'customer.subscription.updated':
+        case 'customer.subscription.deleted':
+          const subscription = event.data.object as Stripe.Subscription;
+          await manageSubscriptionStatusChange(
+            subscription.id,
+            subscription.customer as string,
+            event.type === 'customer.subscription.created'
+          );
+          break;
+        case 'checkout.session.completed':
+          const checkoutSession = event.data.object as Stripe.Checkout.Session;
+          if (checkoutSession.mode === 'subscription') {
+            const subscriptionId = checkoutSession.subscription as string;
+            await manageSubscriptionStatusChange(
+              subscriptionId as string,
+              checkoutSession.customer as string,
+              true
+            );
+          }
+          break;
+        default:
+          throw new Error('Unhandled relevant event');
+      }
+    } catch (error: any) {
+      console.error(error);
+      return new NextResponse(`Webhook error: ${error.message}`, {
+        status: 400,
+      });
+    }
+  }
+
+  return NextResponse.json({ received: true }, { status: 200 });
+}
